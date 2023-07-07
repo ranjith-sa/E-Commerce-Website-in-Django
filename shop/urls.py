f4a36335e1bb9589469b33a8d2d342eb6280ff2f
ranjithrbz <rsranjith2012@gmail.com>
2023-07-07 19:12:52 +0530

    file upload

---
+++ shop/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path 
+from . import views
+
+urlpatterns= [
+    path('',views.home,name="home"),
+    path('register',views.register,name="register"),
+    path('login',views.login_page,name="login"),
+    path('logout',views.logout_page,name="logout"),
+    path('cart',views.cart_page,name="cart"),
+    path('fav',views.fav_page,name="fav"),
+    path('favviewpage',views.favviewpage,name="favviewpage"),
+    path('remove_fav/<str:fid>',views.remove_fav,name="remove_fav"),
+    path('remove_cart/<str:cid>',views.remove_cart,name="remove_cart"),
+    path('collections',views.collections,name="collections"),
+    path('collections/<str:name>',views.collectionsview,name="collections"),
+    path('collections/<str:cname>/<str:pname>',views.product_details,name="product_details"),
+    path('addtocart',views.add_to_cart,name="addtocart"),
+]
